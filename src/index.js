6bf7f5e32d076cb829b2a37d953cad86c4c909f4
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-05-21 19:46:48 +0200

    Add variable, and function.

--- src/index.js
+++ src/index.js
@@ -6,3 +6,13 @@ const newGame = document.getElementById('name');
 const scores = document.getElementById('score')
 const listContainer = document.querySelector('.scores-list')
 const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/'
+
+const renderScore = (gameUsers) => {
+    const gamers = (gameUsers.result);
+    let list = '';
+    gamers.forEach((gameUser) => {
+      list += `<span>${gameUser.user} : ${gameUser.score}</span>`;
+      scoreDiv.innerHTML = list;
+    });
+  };
+  
