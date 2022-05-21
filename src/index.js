00c005ee085efc393ab97ffbf60ad66d20cfe50d
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-05-21 19:44:08 +0200

    Add url and new variables.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import './style.css';
 
 const refreh = document.querySelector('.refresh');
-
+const gameContainer = document.getElementById('form');
+const newGame = document.getElementById('name');
+const scores = document.getElementById('score')
+const listContainer = document.querySelector('.scores-list')
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/'
