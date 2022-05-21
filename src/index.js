836fc9d7530dffc69835d52c11edb9b207c9fb85
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-05-21 20:05:57 +0200

    Update feature.

--- src/index.js
+++ src/index.js
@@ -4,15 +4,14 @@ const refresh = document.querySelector('.refresh');
 const gameContainer = document.getElementById('form');
 const newScoreler = document.getElementById('name');
 const scores = document.getElementById('score')
-// const listContainer = document.querySelector('.scores-list')
-const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/'
-
+const listContainer = document.querySelector('.scores-list')
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Cvjvaty8v75lgFTlmVzh/scores/';
 const renderScore = (gameUsers) => {
     const gamers = (gameUsers.result);
     let list = '';
     gamers.forEach((gameUser) => {
       list += `<span>${gameUser.user} : ${gameUser.score}</span>`;
-      scoreDiv.innerHTML = list;
+      listContainer.innerHTML = list;
     });
   };
   
